5fd85fdc518a84bb02425ff5ba72ca0c6632b527
nilesh <nileshadk@gmail.com>
2018-05-16 15:49:31 +0545

    First commit. Can lock the phone programmatically

---
+++ app/src/main/java/com/repomean/poweroff/MyAdminReceiver.java
@@ -0,0 +1,10 @@
+package com.repomean.poweroff;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by hpdm4 on 5/16/2018.
+ */
+
+public class MyAdminReceiver extends  DeviceAdminReceiver{
+}
